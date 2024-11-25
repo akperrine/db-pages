d85cca39194be69e39f59b6ad2294b42d35a3c1e
Austin Perrine <akperrine@gmail.com>
2024-11-25 07:47:48 -0800

    create inital 2 processes to complete for db

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <string>
 
 int main() {
     std::cout << "printing main" << '\n';
+    bool is_on = true;
+
+    std::cout << "give a command\n";
+    while (is_on) {
+        std::string command;
+        
+        std::cin >> command;
+
+        if (command == "exit") {
+            std::cout << "now exiting program ...\n";
+            is_on = false;
+        }
+    }
+
     return 0;
 }
+
---
+++ src/table.hpp
@@ -0,0 +1,9 @@
+#include "table.cpp";
+
+class Table {
+public:
+    void create_db();
+    void drop_db();
+private:
+
+};
