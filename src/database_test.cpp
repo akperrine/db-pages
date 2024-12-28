7002001a78858d2028b82f64b2360fce17b3e674
Austin Perrine <akperrine@gmail.com>
2024-12-28 10:08:24 -0700

    add cleanup to tests in create db test

--- src/database_test.cpp
+++ src/database_test.cpp
@@ -6,13 +6,31 @@
 
 
 namespace database {
-    TEST(TestDatabase, TestCreateDB) {
-        std::string filename = "test_file"; 
+
+static const std::string DB_NAME = "test_file";
+
+class TestDatabase : public ::testing::Test {
+protected:
+    void TearDown() override {
+        std::string filename = DB_NAME;
+        std::filesystem::remove(std::filesystem::current_path() / "data" / (DB_NAME + ".db")); 
+    }
+};
+
+    TEST_F(TestDatabase, TestCreateDB) {
+        std::string filename = DB_NAME; 
         Database db;
         
+        try {
+
         db.create_db(filename);
-        std::ifstream file(std::filesystem::current_path() / "data" / (filename + ".db"));
+        std::ifstream file(std::filesystem::current_path() / "data" / (DB_NAME + ".db"));
         EXPECT_TRUE(file.is_open()); 
         file.close(); 
+        } catch (...) {
+            // std::filesystem::remove(std::filesystem::current_path() / "data" / (filename + ".db")); 
+        }
+
+    //     std::filesystem::remove(std::filesystem::current_path() / "data" / (filename + ".db")); 
     }
 }
